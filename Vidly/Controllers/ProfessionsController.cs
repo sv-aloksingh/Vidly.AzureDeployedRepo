18795ed685cd691640a5b5d790aa9fbe18b9e6d5
Alok Singh <aloksingh8979@gmail.com>
2020-03-26 00:38:43 +0530

    Added controller
    Added controller

---
+++ Vidly/Controllers/ProfessionsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class ProfessionsController : Controller
+    {
+        // GET: Professions
+        public ActionResult Courses()
+        {
+            return View();
+        }
+
+        public ActionResult Tutorials()
+        {
+            return View();
+        }
+
+        public ActionResult Youtube()
+        {
+            return View();
+        }
+    }
+}
---
+++ Vidly/Controllers/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
