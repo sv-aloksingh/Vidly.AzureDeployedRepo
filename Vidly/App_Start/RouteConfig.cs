fb826b137492d6df96a2393c821ae290a15cd9ed
Alok Singh <aloksingh8979@gmail.com>
2020-03-25 22:40:40 +0530

    footer default page load
    footer default page load

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Vidly
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Customers", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
