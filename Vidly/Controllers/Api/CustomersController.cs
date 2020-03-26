21d0ed2aa320c6134e5decc8d1638dac4579d406
Alok Singh <aloksingh8979@gmail.com>
2020-03-26 21:22:12 +0530

    customers and movies api service added fro Vidly app
    customers and movies api service added fro Vidly app

--- Vidly/Controllers/Api/CustomersController.cs
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -22,6 +22,69 @@ namespace Vidly.Controllers.Api
             _context.Dispose();
         }
 
+        //GET /api/customers
+        [HttpGet]
+        public IEnumerable<Customer> GetCustomer()
+        {
+            var customer = _context.Customers.ToList();
+            return customer;
+        }
+
+        //GET /api/customers/1
+        [HttpGet]
+        public Customer GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c=>c.Id == id);
+
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            return customer;
+        }
+
+        //POST /api/customers/customersData
+        [HttpPost]
+        public Customer CreateCustomer(int id, Customer customer)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault( c=> c.Id == id);
+
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            _context.Customers.Add(customerInDb);
+            _context.SaveChanges();
+            return customerInDb;
+        }
+
+
+        //PUT /api/customers/1
+        [HttpPut]
+        public Customer UpdateCustomer(int id)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var customer = _context.Customers.SingleOrDefault( c=> c.Id == id);
+
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.SaveChanges();
+            return customer;
+        }
+
+
+        //DELETE /api/customers/1
+        [HttpDelete]
+        public Customer DeleteCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault( c=>c.Id == id);
+            
+            
+            _context.Customers.Remove(customer);
+            _context.SaveChanges();
+            return customer;
+        }
 
     }
 }
--- Vidly/Controllers/Api/MoviesController.cs
+++ Vidly/Controllers/Api/MoviesController.cs
@@ -20,5 +20,56 @@ namespace Vidly.Controllers.Api
         {
             _context.Dispose();
         }
+
+        //GET /api/movie
+        [HttpGet]
+        public IEnumerable<Movie> GetMovies()
+        {
+            var movie = _context.Movies.ToList();
+
+
+            return movie;
+        }
+
+        //GET /api/movie/1
+        [HttpGet]
+        public Movie GetMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault( c => c.Id == id);
+
+
+            return movie;
+        }
+
+        //POST /api/movie/
+        [HttpPost]
+        public Movie CreateMovie(int id, Movie movie)
+        {
+            var movieInDb = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            _context.Movies.Add(movieInDb);
+            _context.SaveChanges();
+            return movieInDb;
+        }
+
+        //PUT /api/movie/1
+        [HttpPut]
+        public Movie UpdateMovie(int id)
+        {
+            var movieInDb = _context.Movies.SingleOrDefault( c=> c.Id == id);
+
+            _context.SaveChanges();
+            return movieInDb;
+        }
+
+        //DELETE /api/movie/1
+        [HttpDelete]
+        public Movie DeleteMovie(int id)
+        {
+            var movieInDb = _context.Movies.SingleOrDefault( c=> c.Id == id);
+
+            _context.SaveChanges();
+            return movieInDb;
+        }
     }
 }
