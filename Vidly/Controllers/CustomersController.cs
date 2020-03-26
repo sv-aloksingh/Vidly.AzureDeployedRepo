fa7075dfd6c18380166911a23f3f4fd28b7c2dff
Alok Singh <aloksingh8979@gmail.com>
2020-03-26 17:25:47 +0530

    Added context and tested db connections
    Added context and tested db connections

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
-            return View();
+            var customers = _context.Customers.SingleOrDefault(c=>c.Id == 1);
+            return View(customers);
         }
 
         public ActionResult CustomerProfile()
