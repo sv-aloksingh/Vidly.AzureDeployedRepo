0086da39ddf6dc4dedf0069fce53c9a9c3fb1e8e
Alok Singh <aloksingh8979@gmail.com>
2020-03-26 16:32:33 +0530

    enabled migration and APp connected db
    enabled migration and APp connected to db

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -7,5 +7,7 @@ namespace Vidly.Models
 {
     public class Customer
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -7,5 +7,7 @@ namespace Vidly.Models
 {
     public class Movie
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
